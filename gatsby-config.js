6fb4fb39d8b14a9232540eb42f5935d3229a949d
Bea Malsky <beamalsky@gmail.com>
2019-09-20 13:33:56 -0500

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
